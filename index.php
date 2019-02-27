66f267ce11733238a7c9c90048c7d49993226458
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2019-02-27 10:45:23 +0100

    Created a sample SMS sending script.

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once __DIR__."/Config.php";
+
+$url = "https://rest.nexmo.com/sms/json";
+$postData = array(
+    "api_key" => API_KEY,
+    "api_secret" => API_SECRET,
+    "to" => "387603383856",
+    "from" => "NEXMO",
+    "text" => "Omnia mea mecum porto."
+);
+
+$handler = curl_init();
+
+curl_setopt($handler, CURLOPT_URL, $url);
+curl_setopt($handler, CURLOPT_POSTFIELDS, http_build_query($postData));
+curl_setopt($handler, CURLOPT_POST, true);
+curl_setopt($handler, CURLOPT_SSL_VERIFYPEER, false);
+
+$response = curl_exec($handler);
+
+print_r($response);
