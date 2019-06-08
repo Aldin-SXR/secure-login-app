f627b359392370a1d50cdc48a81e67fcef2e9148
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2019-06-08 18:19:19 +0200

    Updated folder contents.

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// ccccccgcckiiehlbbgtdkgcerkrbcnibklhlleenfcfj
-require_once 'vendor/autoload.php';
-require_once 'api/config/config.php';
-
-$apiKey = 'dGVzdGluZzEyMzQ1Njc4OTA=';
-$clientId = '12345';
-
